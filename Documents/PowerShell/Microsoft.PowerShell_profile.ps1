a6a4c6388cbb89132eef51c9f45f7881a2db3fd4
Purple4pur <purple4pur@gmail.com>
2022-03-13 18:08:15 +0800

    update powershell function

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -10,3 +10,13 @@ Function glg {
     $a = $args -join " "
     git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset' --date=short $a
 }
+
+Function gad {
+    $a = $args -join " "
+    git add $a
+}
+
+Function gcmt {
+    $a = $args -join " "
+    git commit $a
+}
