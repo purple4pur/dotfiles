1ec4535cfea57ed23e2c99e4a4bf2e54b71c72b7
Purple4pur <purple4pur@gmail.com>
2022-03-19 13:25:31 +0800

    update fish config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,11 @@
+starship init fish | source
+
 set -U fish_greeting
 
 set -gx NEMU_HOME /home/purple/workspace/ysyx-workbench/nemu
 set -gx AM_HOME /home/purple/workspace/ysyx-workbench/abstract-machine
+set -gx VERILATOR_ROOT /home/purple/opt/verilator
+set -gx NVBOARD_HOME /home/purple/workspace/ysyx-workbench/nvboard
 
-starship init fish | source
+fish_add_path /usr/lib/ccache
+fish_add_path /home/purple/.local/bin
---
+++ dot_config/private_fish/functions/pipins.fish
@@ -0,0 +1,3 @@
+function pipins
+  pip install -i https://pypi.tuna.tsinghua.edu.cn/simple $argv; 
+end
