66f852c92ad69490b6cc37de5d27160caad2bd6a
Purple4pur <purple4pur@gmail.com>
2023-11-21 13:11:06 +0800

    (vim) fix not setting up glow.nvim

--- dot_config/nvim/lua/init.lua
+++ dot_config/nvim/lua/init.lua
@@ -4,3 +4,13 @@ vim.o.guifont = 'Sarasa_Term_SC_Nerd:h11,终端更纱黑体-简_Nerd:h11'
 -- keymap
 vim.keymap.set('n', '<leader>T', '<cmd>tabnew +terminal<CR>', { silent = true })
 vim.keymap.set('t', '<C-\\>', '<C-\\><C-N>')
+
+-- plugins
+vim.opt.runtimepath:append(vim.fn.stdpath('config') .. '/lua/glow.nvim')
+require('glow').setup({
+    border = 'none',
+    width = 150,
+    height = 100,
+    width_ratio = 0.8,
+    height_ratio = 0.8,
+})
