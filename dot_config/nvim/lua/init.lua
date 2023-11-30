4cc056a581d278f4627087d87095f36ca12ce09d
Purple4pur <purple4pur@gmail.com>
2023-11-30 20:57:34 +0800

    (vim) setup mini.nvim

--- dot_config/nvim/lua/init.lua
+++ dot_config/nvim/lua/init.lua
@@ -15,3 +15,23 @@ require('glow').setup({
     height_ratio = 0.8,
 })
 vim.keymap.set('n', '<leader>p', '<cmd>Glow<CR><cmd>nohlsearch<CR>')
+
+vim.opt.runtimepath:append(vim.fn.stdpath('config') .. '/lua/mini.nvim')
+require('mini.completion').setup()
+require('mini.cursorword').setup()
+require('mini.files').setup() -- `<leader>e<e/v/t>` to open
+require('mini.indentscope').setup()
+require('mini.jump2d').setup() -- `<CR>` to enter jump mode
+require('mini.map').setup({ window = { focusable = true } }) -- `<leader>mm` to toggle
+require('mini.move').setup() -- `<M-h/j/k/l>` to move
+require('mini.pairs').setup()
+require('mini.splitjoin').setup() -- `gS` to toggle
+require('mini.starter').setup()
+require('mini.statusline').setup()
+require('mini.trailspace').setup() -- `<leader>dz` to remove trailspaces/lines
+
+vim.keymap.set('n', '<leader>ee', '<cmd>lua MiniFiles.open()<CR>')
+vim.keymap.set('n', '<leader>ev', '<cmd>vnew<CR><cmd>lua MiniFiles.open()<CR>')
+vim.keymap.set('n', '<leader>et', '<cmd>tabnew<CR><cmd>lua MiniFiles.open()<CR>')
+vim.keymap.set('n', '<leader>mm', '<cmd>lua MiniMap.toggle()<CR>')
+vim.keymap.set('n', '<leader>dz', '<cmd>lua MiniTrailspace.trim()<CR><cmd>lua MiniTrailspace.trim_last_lines()<CR>')
