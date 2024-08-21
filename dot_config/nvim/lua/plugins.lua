b0325af92d18377d185879828eeadd0557cb44e4
Purple4pur <purple4pur@gmail.com>
2024-08-22 00:25:00 +0800

    (vim) enable mini.diff; customize mini.statusline trunc_width

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -13,6 +13,7 @@ vim.keymap.set('n', '<leader>p', '<cmd>Glow<CR><cmd>nohlsearch<CR>')
 
 require('mini.completion').setup()
 require('mini.cursorword').setup()
+require('mini.diff').setup()
 
 -- <leader>e<e/v/t>: open file explorer
 require('mini.files').setup()
@@ -53,7 +54,31 @@ require('mini.move').setup()
 require('mini.splitjoin').setup()
 
 require('mini.starter').setup()
-require('mini.statusline').setup()
+require('mini.statusline').setup({
+    content = {
+        active = function()
+            local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 75 })
+            local git           = MiniStatusline.section_git({ trunc_width = 40 })
+            local diff          = MiniStatusline.section_diff({ trunc_width = 75 })
+            local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+            local lsp           = MiniStatusline.section_lsp({ trunc_width = 75 })
+            local filename      = MiniStatusline.section_filename({ trunc_width = 90 })
+            local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 75 })
+            local location      = MiniStatusline.section_location({ trunc_width = 75 })
+            local search        = MiniStatusline.section_searchcount({ trunc_width = 75 })
+            return MiniStatusline.combine_groups({
+                { hl = mode_hl,                  strings = { mode } },
+                { hl = 'MiniStatuslineDevinfo',  strings = { git, diff, diagnostics, lsp } },
+                '%<', -- Mark general truncate point
+                { hl = 'MiniStatuslineFilename', strings = { filename } },
+                '%=', -- End left alignment
+                { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+                { hl = mode_hl,                  strings = { search, location } },
+            })
+        end,
+    },
+    use_icons = false,
+})
 
 -- s<a/d/r/h/f/F>: add/delete/replace/highlight/f(F)ind
 require('mini.surround').setup()
