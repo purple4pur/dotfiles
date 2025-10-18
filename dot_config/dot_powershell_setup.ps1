fe834a5d8962b8b8e5a0fedb3997c878136bad1d
Purple4pur <49893724+purple4pur@users.noreply.github.com>
2025-10-18 16:14:20 +0800

    (bash,tcsh,powershell) handle starship downgrade; remove --dir-diff in 'gdt'

--- dot_config/dot_powershell_setup.ps1
+++ dot_config/dot_powershell_setup.ps1
@@ -1,30 +1,15 @@
-# ----------
-#   Common
-# ----------
 $EDITOR = if (Test-Path variable:EDITOR) { $EDITOR } else { "notepad.exe" }
 Function vims { & $EDITOR $PROFILE $args }
 
-# -------------
-#   Git Alias
-# -------------
 Function gad { & git add $args }
 Function gb { & git branch $args }
 Function gcmt { & git commit $args }
 Function gco { & git checkout $args }
 Function gd { & git diff $args }
-Function gdt { & git difftool --dir-diff $args }
+Function gdt { & git difftool $args }
 Function glg { & git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset' --date=short $args }
 Function gst { & git status $args }
 
-# ------------
-#   Starship
-# ------------
-Invoke-Expression (&starship init powershell)
-
-# ----------
-#   Zoxide
-# ----------
-#Invoke-Expression (& {
-#    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
-#    (zoxide init --hook $hook powershell | Out-String)
-#})
+if (Get-Command starship -ErrorAction SilentlyContinue) {
+    Invoke-Expression (&starship init powershell)
+}
